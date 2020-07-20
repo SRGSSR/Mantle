791a8c1cb92fe1c3ea925e30f37a1e2251b9bcec
Samuel Défago <defagos@gmail.com>
2020-07-20 15:27:16 +0200

    Make project compile as a package

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Mantle",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v8),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "Mantle",
+            targets: ["Mantle"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Mantle",
+            dependencies: ["extobjc"],
+            path: "Mantle",
+            exclude: [
+                "extobjc"
+            ],
+            publicHeadersPath: "."
+        ),
+        .target(
+            name: "extobjc",
+            path: "Mantle/extobjc",
+            publicHeadersPath: "."
+        )
+    ]
+)
